80779555934dd067fa3a2c939111027e01d583f0
globalafrican <nzanzuh@gmail.com>
2022-04-23 15:00:47 +0100

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Home(){
+    return (
+        <>
+            <Head>
+                <title>Herve Portfolio</title>
+                <meta name="description" content="Portfolio Web Site of Nzanzu Herve" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <main>Hello World</main>
+      </>
+    )
+}
